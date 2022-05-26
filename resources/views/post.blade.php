2dcf6bfd6f9040d61f476fa2c96baeec4379c3e3
adityafirnanda <firnandaaditya2236@gmail.com>
2022-05-26 15:04:33 +0800

    Templating Engine Blade

---
+++ resources/views/post.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.main')
+
+@section('container')
+    <article>
+        <h2>Judul</h2>
+        <h5>Author</h5>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa mollitia soluta praesentium quod corrupti expedita neque incidunt architecto distinctio totam repudiandae, sapiente ad omnis eaque quos adipisci non porro ab.</p>
+    </article>
+@endsection
