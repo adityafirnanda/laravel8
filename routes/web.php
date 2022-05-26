3f048537db99d27041b1289215b86669808f2914
adityafirnanda <firnandaaditya2236@gmail.com>
2022-05-26 09:37:34 +0800

    Blade Templating Engine

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,17 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+
+Route::get('/about', function () {
+    return view('about',[
+        "name" => "Aditya Firnanda",
+        "email" => "firnandaaditya2236@gmail.com",
+        "image" => "eren.jpg"
+    ]);
+});
+
+Route::get('/blog', function () {
+    return view('posts');
 });
